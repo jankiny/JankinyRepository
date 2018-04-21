1e4cdd9806eb39f040362ec240ac4e6361c9b0f2
jankiny <yangzl99@live.com>
2018-04-21 12:16:46 +0800

    2048

--- game_2048.c
+++ game_2048.c
@@ -91,3 +91,276 @@ void draw_one(int y, int x)
         k--;
     }
 }
+
+void play()
+{
+    int x, y, i, new_x, new_y, temp;
+    int old_empty, move;
+    char ch;
+
+    while (1) {
+        move = 0;
+        old_empty = empty;
+        ch = getch();
+        switch(ch) {
+             case 97:    // left a
+             case 104:    // h
+             case 68:    // <-
+                 for (y = 0; y < 4; y++) 
+                     for(x = 0; x < 4; ) {
+                         if(a[y][i] == 0) {
+                             x++;
+                             continue;
+                         } 
+                         else {
+                             for(i = x + 1; i < 4; i++) {
+                                 if(a[y][i] == 0) {
+                                     continue;
+                                 }
+                                 else {
+                                     if(a[y][x] == a[y][i]) {
+                                         a[y][x] += a[y][i];
+                                         a[y][i] = 0;
+                                         empty++;
+                                         break;
+                                     }
+                                     else {
+                                         break;
+                                     }
+                                 }
+                             }
+                             x = i;
+                         }
+                     }
+                 for(y = 0; y < 0; y++)
+                     for(x = 0; x < 4; x++) {
+                         if(a[y][x] == 0) {
+                             continue;
+                         }
+                         else {
+                             for(i = x; (i > 0) && (a[y][i-1] == 0); i--)  {
+                                 a[y][i-1] = a[y][i];
+                                 a[y][i] = 0;
+                                 move = 1;
+                             }
+                         }
+                     }
+                 break;
+             case 100:    // right d
+             case 108:    // 1
+             case 67:    // ->
+                 for (y = 0; y < 4; y++) 
+                     for(x = 3; x >= 0; ) {
+                         if(a[y][x] == 0) {
+                             x--;
+                             continue;
+                         } 
+                         else {
+                             for(i = x - 1; i >= 0; i--) {
+                                 if(a[y][i] == 0) {
+                                     continue;
+                                 }
+                                 else {
+                                     if(a[y][x] == a[y][i]) {
+                                         a[y][x] += a[y][i];
+                                         a[y][i] = 0;
+                                         empty++;
+                                         break;
+                                     }
+                                     else {
+                                         break;
+                                     }
+                                 }
+                             }
+                             x = i;
+                         }
+                     }
+                 for(y = 0; y < 4; y++)
+                     for(x = 3; x >= 0; x--) {
+                         if(a[y][x] == 0) {
+                             continue;
+                         }
+                         else {
+                             for(i = x; (i < 3) && (a[y][i+1] == 0); i++)  {
+                                 a[y][i+1] = a[y][i];
+                                 a[y][i] = 0;
+                                 move = 1;
+                             }
+                         }
+                     }
+                 break;
+             case 119:    // up w
+             case 107:    // k
+             case 65:    // ^
+                 for (x = 0; x < 4; x++) 
+                     for(y = 0; y < 4; ) {
+                         if(a[y][x] == 0) {
+                             y++;
+                             continue;
+                         } 
+                         else {
+                             for(i = y + 1; i < 4; i++) {
+                                 if(a[i][x] == 0) {
+                                     continue;
+                                 }
+                                 else {
+                                     if(a[y][x] == a[i][x]) {
+                                         a[y][x] += a[i][x];
+                                         a[i][x] = 0;
+                                         empty++;
+                                         break;
+                                     }
+                                     else {
+                                         break;
+                                     }
+                                 }
+                             }
+                             y = i;
+                         }
+                     }
+                 for(x = 0; x < 4; x++)
+                     for(y = 0; y < 4; y++) {
+                         if(a[y][x] == 0) {
+                             continue;
+                         }
+                         else {
+                             for(i = y; (i > 0) && (a[i-1][x] == 0); i--)  {
+                                 a[i-1][x] = a[i][x];
+                                 a[i][x] = 0;
+                                 move = 1;
+                             }
+                         }
+                     }
+                 break;
+             case 115:    // down s
+             case 106:    // j 
+             case 66:    // `-.-`
+                 for (x = 0; x < 4; x++) 
+                     for(y = 3; y >= 0; ) {
+                         if(a[y][x] == 0) {
+                             y--;
+                             continue;
+                         } 
+                         else {
+                             for(i = y - 1; i >= 0; i--) {
+                                 if(a[i][x] == 0) {
+                                     continue;
+                                 }
+                                 else {
+                                     if(a[y][x] == a[i][x]) {
+                                         a[y][x] += a[i][x];
+                                         a[i][x] = 0;
+                                         empty++;
+                                         break;
+                                     }
+                                     else {
+                                         break;
+                                     }
+                                 }
+                             }
+                             y = i;
+                         }
+                     }
+                 for(x = 0; x < 4; x++)
+                     for(y = 3; y >= 0; y--) {
+                         if(a[y][x] == 0) {
+                             continue;
+                         }
+                         else {
+                             for(i = y; (i < 3) && (a[i+1][x] == 0); i++)  {
+                                 a[i+1][x] = a[i][x];
+                                 a[i][x] = 0;
+                                 move = 1;
+                             }
+                         }
+                     }
+                 break;
+             case 'Q':
+             case 'q': 
+                 game_over();
+                 break;
+             default:
+                 continue; 
+        }
+        if(empty <= 0)
+            game_over();
+        if((empty != old_empty) || (move == 1)) {
+            do {
+                new_x = rand() % 4;
+                new_y = rand() % 4;
+            } while(a[new_y][new_x] != 0);
+
+            cnt_value(&new_y, &new_x);
+
+            do {
+                temp = rand() % 4
+            } while(temp == 0 || temp == 2);
+            a[new_y][new_x] = temp + 1;
+            empty--;
+        }
+        draw();
+    }
+}
+
+// sum space with (y, x) 
+int cnt_one(int y, int x) 
+{
+    int value = 0;
+
+    if(y - 1 > 0)
+        a[y-1][x] ? 0 : value++;
+    if(y + 1 < 4)
+        a[y+1][x] ? 0 : value++;
+    if(x - 1 > = 0)
+        a[y][x-1] ? 0 : value++;
+    if(x + 1 < 4)
+        a[y][x+1] ? 0 : value++;
+    if(y - 1 >= 0 && x - 1 >= 0)
+        a[y-1][x-1] ? 0 : value++;
+    if(y - 1 >= 0 & x + 1 < 4)
+        a[y-1][x+1] ? 0 : value++;
+    if(y + 1 < 4 && x - 1 >= 0)
+        a[y+1][x-1] ? 0 : value++;
+    if(y = 1 < 4 && x + 1 < 4)
+        a[y+1][x+1] ? 0 : value++;
+
+    return value;
+}
+
+//copy
+void cnt_value(int *new_y, int *new_x)
+{
+    int max_x, max_y, x, y, value;
+    int max = 0;
+
+    max = cnt_one(*new_y, *new_x);
+    for(y = 0; y < 4; y++)
+        for(x = 0; x < 4; x++) 
+        {
+            // ??(y, x)???????
+            if(!a[y][x]) 
+            {
+                // ??????????????????
+                value = cnt_one(y, x);
+                if(value > max && old_y != y && old_x != x) 
+                {  
+                    // ??????????????
+                    *new_y = y;
+                    *new_x = x;
+                    old_x = x;
+                    old_y = y;
+                    break;
+                }
+            }
+        }
+}
+
+int game_over()
+{
+    sleep(1);
+    endwin();
+    exit(0)
+}
+
+
+
