f2543eac405fe5beaf8f2e9f746ca4668e34b138
jankiny <yangzl99@live.com>
2018-04-23 22:53:55 +0800

    have drawn frame

---
+++ 2048_v1/2048-v1.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "cursor.h"
+
+char **cvas;    // Canvas
+int a[4], b[4];    // save cooedinate
+int emp[16];    // use to product number in empty position
+
+void init();    // initialization
+char randN(int m);    // return 4 or 2, 'm' mean probability of '4'
+void judge_empty();    //judge empty position
+void product_number();    // product number in empty position
+
+int main() {
+    init();
+    refresh();
+}
+
+// create a canvas, and draw line, product begining number
+void init() {
+    int i, j;
+    int temp;
+
+    // draw frame
+    cvas = canvas(7, 15);    //need line x:2 4 6 y:4 8 12
+    for(i = 1; i <= 7; i++) {
+        for(j = 4; j <= 12; j += 4) {
+            cursor_move(i, j);
+            addch('|');
+        }
+    }
+    for(i = 2; i <= 6; i += 2) {
+        for(j = 1; j <= 15; j++) {
+            cursor_move(i, j);
+            addch('-');
+        }
+    }
+
+    // associated digital coordinates
+    for(temp = 0, i = 1; i <= 7; i += 2) {
+        a[temp++] = i;
+    }
+    for(temp = 0, j = 3; j <= 15; j += 4) {
+        b[temp++] = j;
+    }
+
+    judge_empty();
+    product_number();
+/* problem: if add this function, since of the time is too short,
+            so, it would create two same numbers, like double 2 or 4.
+
+    product_number();
+*/
+}
+
+char randN(int m) {    // m% return 4
+    int n;
+
+    srand((unsigned)time(NULL));
+    n = rand() % 100;
+    if(n <= m) {
+        return '4';
+    }
+    else
+        return '2';
+}
+
+// judge empty pisition
+void judge_empty() {
+    int i, j;
+
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4; j++) {
+            if(cvas[*(a+i)][*(b+j)] == ' ') {
+                emp[i*4+j] = 0;
+            }
+        }
+    }
+}
+
+void product_number() {
+    int n;
+
+    srand((int)time(NULL));
+    do {
+        n = rand() % 16;
+    } while(emp[n]);    // if emp(n) is empty, out
+/* degub code:  
+    show_iarray(emp, 16);
+    printf(" N:%d \n", n);   */
+    cursor_move(*(a+n/4), *(b+n%4));
+    addch(randN(50));    // 50 is probability
+    emp[n] = 1;
+}
+
--- 2048_v1/cursor.h
+++ 2048_v1/cursor.h
@@ -1,10 +1,10 @@
-/* cursor handle need to include: */
+/* cursor handle need to include: stdio.h, stdlib.h */
 
 /* principles: First, we create a Canvas(define a rectangular space),   
                it was full of ' '(space), and surranded by black lines. 
                Then, we can draw the Canvas with character and cursor. */
 
-/* As for the direction, x is pointing to right, and y is pointing to down */
+/* As for the direction, x is pointing to down, and y is pointing to right */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +17,13 @@ int range_y = 0;
 char **cvas;
 int mode = 2; // 1(use): hide detials; 2(debug):show detials
 
+// test int array
+void show_iarray(int *a, int n) {
+    int i;
+
+    for(i = 0; i < n; i++)
+        printf("%d ", *(a+i));
+}
 // create a Canvas
 char **canvas(int a, int b) {
     int i, j;
@@ -55,7 +62,8 @@ void delch() {
 */
 void cursor_move(int a, int b) {
     if(a == 0 || b == 0 || a >= range_x || b >= range_y) {
-        printf("Out of range! \nexit \n");
+        printf("cursor move out of range! \n");
+        printf("(%d, %d) in range(1-%d,1-%d) \n", a, b, range_x, range_y);
         exit(1);
     }
     x = a;
@@ -66,7 +74,8 @@ void char_move(int a, int b) {
     char temp;
 
     if(a == 0 || b == 0 || a >= range_x || b >= range_y) {
-        printf("Out of range! \nexit \n");
+        printf("character move out of range! \n");
+        printf("(%d, %d) in range(1-%d,1-%d) \n", a, b, range_x, range_y);
         exit(1);
     }
     temp = cvas[x][y];
@@ -84,6 +93,9 @@ void refresh() {
     for(i = 0; i <= range_x; i++) {
         for(j = 0; j <= range_y; j++) {
              printf("%c", cvas[i][j]);
+        }
+        if(mode == 2) {
+            printf("%2d", i);
         }
         putchar('\n');
     }
